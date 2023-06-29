215f2107ec91c170eb08ee995a608f61aed9f5df
foxcraftYT <rodriguezdylan041@gmail.com>
2023-06-28 21:07:08 -0300

    pito corto

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> Incrustar PHP en un archivo .html </title>
+    </head>
+    <body>
+        <h1><?php echo "Hola Mundo" ?></h1>
+    </body>
+</html>
